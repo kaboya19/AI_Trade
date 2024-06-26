4820b3cc2de849baa9970812d44247b3063e295f
Bora Kaya <kayabo19@itu.edu.tr>
2024-06-27 00:54:26 +0300

    .

--- streamlit_model.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import quantstats as qs
-import streamlit as st
-bist100returns=pd.read_csv("bist100returns.csv")
-st.set_page_config(page_title="AI Trading Model Takip Sayfası")
-fig = qs.plots.snapshot(bist100returns,show=False)
-st.pyplot(fig)
-
